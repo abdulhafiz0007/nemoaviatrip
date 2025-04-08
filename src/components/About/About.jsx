69194ce0c5dfd4818e065c71594cb5c0ffac2764
abdulhafiz0007 <mrsteves77@gmail.com>
2025-04-08 18:06:42 +0500

    add animation to footer

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,59 +10,53 @@ export const About = () => {
    const { til } = useContext(LangContext);
 
    return (
-      <div 
-         id="about" 
-         className="lg:w-[1487px] m-auto scroll-mt-[120px] px-[20px]" 
-         whileInView={{ opacity: 1 }} 
-         exit={{opacity: 0}}
-         initial={{ opacity: 0 }} 
+      <div
+         id="about"
+         className="lg:w-[1487px] m-auto scroll-mt-[120px] px-[20px] overflow-x-hidden"
+         whileInView={{ opacity: 1 }}
+         exit={{ opacity: 0 }}
+         initial={{ opacity: 0 }}
          transition={{ duration: 1.5 }}
          viewport={{ once: true }}
       >
          <h2 className="text-[40px] mt-[60px] font-bold text-center mb-[16px]">
             {lang[til].about.title}
          </h2>
-         <p className="text-[22px] text-center">    
-            {lang[til].about.subTitle}
-         </p>
+         <p className="text-[22px] text-center">{lang[til].about.subTitle}</p>
 
          <ul className="flex flex-col lg:flex-row items-center justify-center gap-[58px] mt-[40px]">
-            <li 
+            <li
                className="px-[30px] pr-[40px] lg:w-[719px] lg:h-[160px] m-auto  py-[30px] shadow-xl rounded-xl"
-               whileInView={{ x: 0, opacity: 1 }} 
+               whileInView={{ x: 0, opacity: 1 }}
                initial={{ x: -200, opacity: 0 }}
                transition={{ duration: 1 }}
             >
-               <h3 className="text-[24px] mb-[20px] font-bold">    
+               <h3 className="text-[24px] mb-[20px] font-bold">
                   {lang[til].about.mission}
                </h3>
-               <p className="text-[#4B5563]">
-                  {lang[til].about.missionText}
-               </p>
+               <p className="text-[#4B5563]">{lang[til].about.missionText}</p>
             </li>
 
-            <li 
+            <li
                className="px-[30px] pr-[40px] lg:w-[719px] lg:h-[160px] m-auto  py-[30px] shadow-xl rounded-xl"
-               whileInView={{ x: 0, opacity: 1 }} 
+               whileInView={{ x: 0, opacity: 1 }}
                initial={{ x: 200, opacity: 0 }}
                transition={{ duration: 1 }}
             >
-               <h3 className="text-[24px] mb-[20px] font-bold">    
+               <h3 className="text-[24px] mb-[20px] font-bold">
                   {lang[til].about.vision}
                </h3>
-               <p className="text-[#4B5563]">
-                  {lang[til].about.visionText}
-               </p>
+               <p className="text-[#4B5563]">{lang[til].about.visionText}</p>
             </li>
          </ul>
 
          <div className="flex flex-col lg:flex-row gap-5 items-center justify-between lg:w-[1280px] m-auto mt-[60px]">
-            <div 
+            <motion.div
                className="flex flex-col lg:w-[624px]"
-               whileInView={{ x: 0, opacity: 1 }} 
                initial={{ x: -250, opacity: 0 }}
-               exit={{x: -250, opacity: 0}}
-               transition={{ duration: 1.3, type: "tween" }}
+               whileInView={{ x: 0, opacity: 1 }}
+               transition={{ duration: 1.2, type: "tween" }}
+               viewport={{ once: false, amount: 0.2 }}
             >
                <p className="text-[#4B5563] text-[17px] mb-[16px]">
                   {lang[til].about.aboutDesc1}
@@ -70,14 +64,14 @@ export const About = () => {
                <p className="text-[#4B5563] text-[17px]">
                   {lang[til].about.aboutDesc2}
                </p>
-            </div>
+            </motion.div>
 
-            <div 
+            <motion.div
                className="flex flex-col gap-[16px]"
-               whileInView={{ x: 0, opacity: 1 }} 
-               exit={{x:250, opacity: 0}}
                initial={{ x: 250, opacity: 0 }}
-               transition={{ duration: 1.3, type: "tween" }}
+               whileInView={{ x: 0, opacity: 1 }}
+               transition={{ duration: 1.2, type: "tween" }}
+               viewport={{ once: false, amount: 0.2 }}
             >
                <img
                   className="w-[624px] h-[256px] object-cover rounded-xl"
@@ -102,7 +96,7 @@ export const About = () => {
                      alt="Nemo office img"
                   />
                </div>
-            </div>
+            </motion.div>
          </div>
       </div>
    );
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,48 +4,76 @@ import { TelegramIcon, InstagramIcon } from "../../assets/images/Icons";
 import { lang } from "../Lang/Lang";
 import { useContext } from "react";
 import { LangContext } from "../../context/LangContext";
-import {motion} from 'framer-motion';
+import { motion } from "framer-motion";
 
 export const Footer = () => {
-
-   const {til} = useContext(LangContext);
+   const { til } = useContext(LangContext);
 
    return (
-      <motion.footer className="mt-[30px] bg-[#111827] lg:w-full m-auto">
+      <motion.footer
+         initial={{ y: 100, opacity: 0 }}
+         whileInView={{ y: 0, opacity: 1 }}
+         transition={{ duration: 1, type: "tween" }}
+         viewport={{ amount: 0.2, once: false }}
+         className="mt-[30px] bg-[#111827] lg:w-full m-auto"
+      >
          <div className="lg:w-[1280px] m-auto px-[32px] py-[48px]">
             <ul className="flex flex-col lg:flex-row gap-5 items-start justify-between">
                <li className="flex flex-col gap-[20px]">
-                  <img className="w-[112px] h-[37px]" src={SiteLogo} alt="site logo" />
-                  <p className="text-[#9CA3AF]">{lang[til].footer.footerAddress}</p>
+                  <img
+                     className="w-[112px] h-[37px]"
+                     src={SiteLogo}
+                     alt="site logo"
+                  />
+                  <p className="text-[#9CA3AF]">
+                     {lang[til].footer.footerAddress}
+                  </p>
                </li>
-               <li className="flex flex-col gap-[20px]"> 
-                  <h3  className="text-[20px] text-white font-medium">{lang[til].footer.footerContact}</h3>
+               <li className="flex flex-col gap-[20px]">
+                  <h3 className="text-[20px] text-white font-medium">
+                     {lang[til].footer.footerContact}
+                  </h3>
                   <div className="flex flex-col gap-1">
-                      <a className="text-[#9CA3AF]" href="tel:+998 95 150 00 00">{lang[til].footer.footerTel}</a>
-                      <a className="text-[#9CA3AF]" href="mailto:info@nemoavia.uz">{lang[til].footer.footerEmail}</a>
+                     <a className="text-[#9CA3AF]" href="tel:+998 95 150 00 00">
+                        {lang[til].footer.footerTel}
+                     </a>
+                     <a
+                        className="text-[#9CA3AF]"
+                        href="mailto:info@nemoavia.uz"
+                     >
+                        {lang[til].footer.footerEmail}
+                     </a>
                   </div>
                </li>
                <li className="flex flex-col gap-[20px]">
-                  <h3 className="text-[20px] text-white font-medium" >{lang[til].footer.footerSocialmedia}</h3>
+                  <h3 className="text-[20px] text-white font-medium">
+                     {lang[til].footer.footerSocialmedia}
+                  </h3>
                   <ul className="flex items-center gap-[10px]">
-                    <li>
+                     <li>
                         <TelegramIcon />
-                    </li>
-                    <li>
+                     </li>
+                     <li>
                         <InstagramIcon />
-                    </li>
-                    <li>
+                     </li>
+                     <li>
                         <TelegramIcon />
-                    </li>
+                     </li>
                   </ul>
                </li>
                <li className="flex flex-col gap-[20px]">
-                  <h3 className="text-[20px] text-white font-medium" >Address</h3>
-                  <p className="text-[#9CA3AF]">{lang[til].footer.footerAddress2}</p>
+                  <h3 className="text-[20px] text-white font-medium">
+                     Address
+                  </h3>
+                  <p className="text-[#9CA3AF]">
+                     {lang[til].footer.footerAddress2}
+                  </p>
                </li>
             </ul>
             <span className="block lg:w-[1280px] mt-[32px] border-t-[1.2px] border-[#1F2937]"></span>
-            <p className="text-[#9CA3AF] text-center pt-[32px]">{lang[til].footer.footerCopyRight}</p>
+            <p className="text-[#9CA3AF] text-center pt-[32px]">
+               {lang[til].footer.footerCopyRight}
+            </p>
          </div>
       </motion.footer>
    );
